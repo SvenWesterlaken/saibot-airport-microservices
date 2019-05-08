778b1e73d3f08d1231af531718190a449318d6af
Thimo Koolen <tkoolen22@gmail.com>
2019-05-08 13:47:57 +0200

    Added POST call to add new runway

--- airside_management/routes/v1/runway.v1.js
+++ airside_management/routes/v1/runway.v1.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const amqpManager = require('../../events/amqp.manager');
+const uuid = require('uuid/v4');
 
 router.get('/', (req, res) => {
 	amqpManager.connect()
@@ -11,6 +12,43 @@ router.get('/', (req, res) => {
 			}))
 		});
 	res.status(200).json({message: 'Welcome to Runway V1 API'});
+});
+
+router.post('/', (req, res) => {
+	let body = req.body;
+	
+	//Check for missing properties
+	if (!body.side1 || !body.side2 || !body.length || !body.width) {
+		res.status(400).json({error: 'Invalid request.'});
+	}
+	
+	//TODO Implement logic to add runway to database
+	
+	//Create message payload
+	let payload = {
+		id: uuid(),
+		message: 'New runway has been added successfully.',
+		from: 'airside_management',
+		type: 'CREATE',
+		data: {
+			side1: body.side1,
+			side2: body.side2,
+			length: body.length,
+			width: body.width
+		},
+		old_data: {}
+	}
+	
+	//Send message
+	amqpManager.connect()
+		.then((channel) => {
+			amqpManager.sendMessageToQueue(channel, 'airside-runway', JSON.stringify(payload));
+			res.status(201).json(payload);
+		})
+});
+
+router.delete('/{id}', (req, res) => {
+	
 });
 
 module.exports = router;
