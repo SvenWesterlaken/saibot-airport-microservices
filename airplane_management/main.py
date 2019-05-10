fdc7a778bf905c7b24a985505c15554a31818b44
Twan van Maastricht <ajmaastr@avans.nl>
2019-05-10 11:06:27 +0200

    create app in main

--- airplane_management/main.py
+++ airplane_management/main.py
@@ -5,11 +5,6 @@ from app.rabbitmq import rabbitmq
 app = create_app()
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-
 if __name__ == "__main__":
     rabbitmq.connect()
     app.run(host="0.0.0.0", debug="true", use_reloader=False)
