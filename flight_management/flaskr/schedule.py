4e824f883851dc1f887d9398d24615c66c3e7774
Sven <sven.westerlaken@hotmail.nl>
2019-05-10 14:29:59 +0200

    add flight request endpoint

--- flight_management/flaskr/schedule.py
+++ flight_management/flaskr/schedule.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template
 from pony.orm import *
+from models import Flight, Gate
 import json, arrow
 
 bp = Blueprint('schedule', __name__, url_prefix='/schedule')
@@ -10,6 +11,7 @@ def getSchedule(type):
     params = request.args
 
     now = arrow.now().date()
+    type = bool(int(type))
 
     start_date = arrow.get(now)
     end_date = arrow.get(now).shift(days=+1)
@@ -20,5 +22,33 @@ def getSchedule(type):
     if 'end_date' in params.keys():
         end_date = arrow.get(params['end_date'])
 
+    flights = [f.to_dict() for f in select(f for f in Flight if f.time >= start_date.datetime and f.time <= end_date.datetime and f.type == type)[:]]
 
-    return json.dumps({'start': start_date.format('YYYY-MM-DD'), 'end': end_date.format('YYYY-MM-DD')})
+    return json.dumps({'start': start_date.format('YYYY-MM-DD'), 'end': end_date.format('YYYY-MM-DD'), 'flights': flights})
+
+@bp.route('/request', methods=['POST'])
+@db_session
+def requestFreeGate():
+    flight = json.loads(request.data.decode('UTF-8'))
+
+    is_departing = flight['type'] == 1
+    time = arrow.get(flight['time'], 'YYYY-MM-DD HH:mm:ss')
+
+    flight['time'] = time.datetime
+    flight['type'] = is_departing
+
+    start_time = time.shift(hours=-1) if is_departing else time.shift(minutes=-15)
+    end_time = time.shift(minutes=+15) if is_departing else time.shift(hours=+1)
+
+    overlapping_count = select(f for f in Flight if f.start_time <= end_time.datetime and f.end_time >= start_time.datetime).count()
+    gates_count = Gate.select().count()
+
+    if overlapping_count < gates_count:
+        new_flight = Flight(**flight)
+        commit()
+
+        print(new_flight.start_time)
+
+        return json.dumps(new_flight.to_dict())
+    else:
+        return 'Sorry no free spots available'
--- flight_management/models/flight.py
+++ flight_management/models/flight.py
@@ -9,14 +9,31 @@ class Flight(db.Entity, ParsingMixin):
     location = Required(str)
     airline = Required('Airline')
     airplane = Required('Airplane')
-    time = Required(datetime)
+    start_time = Optional(datetime, volatile=True)
+    time = Required(datetime, volatile=True)
+    end_time = Optional(datetime, volatile=True)
     status = Required(str, default='scheduled')
     gate = Optional('Gate')
-    updated_at = Required(datetime, default=arrow.now().datetime)
-    created_at = Required(datetime, default=arrow.now().datetime)
+    updated_at = Required(datetime, default=arrow.now().datetime, volatile=True)
+    created_at = Required(datetime, default=arrow.now().datetime, volatile=True)
+    deleted_at = Optional(datetime, volatile=True)
+
+    def cancel(self):
+        self.status = 'canceled'
+        deleted_at = arrow.now().datetime
+
+    def set_before_and_after_time(self):
+        time = arrow.get(self.time)
+        is_departing = self.type
+
+        self.start_time = time.shift(hours=-1).datetime if is_departing else time.shift(minutes=-15).datetime
+        self.end_time = time.shift(minutes=+15).datetime if is_departing else time.shift(hours=+1).datetime
 
     def before_update(self):
         self.updated_at = arrow.now().datetime
+
+    def before_insert(self):
+        self.set_before_and_after_time()
 
     def to_dict(self, *args, **kwargs):
         dict = super().to_dict(*args, **kwargs)
@@ -24,7 +41,5 @@ class Flight(db.Entity, ParsingMixin):
         for k,v in dict.items():
             if isinstance(v, datetime):
                 dict[k] = arrow.get(v).format('YYYY-MM-DD HH:mm:ss')
-
-        print(dict)
 
         return dict
