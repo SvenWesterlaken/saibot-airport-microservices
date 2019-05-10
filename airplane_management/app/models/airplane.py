a6cdbe2872e70380d69bef2d0d8abe983a4aaf6d
Twan van Maastricht <ajmaastr@avans.nl>
2019-05-10 16:16:01 +0200

    config file used. MySQL db filled with stubs

---
+++ airplane_management/app/models/airplane.py
@@ -0,0 +1,7 @@
+from pony.orm import *
+from .base import db, ParsingMixin
+
+
+class Airplane(db.Entity, ParsingMixin):
+    max_capacity = Required(int)
+    airline = Required('Airline')
--- airplane_management/config.py
+++ airplane_management/config.py
@@ -1,1 +1,11 @@
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    PROVIDER = 'mysql'
+    HOST = os.environ.get('MYSQL_HOST')
+    USER = os.environ.get('root')
+    PASSWD = os.environ.get('MYSQL_PASS')
+    DB = os.environ.get('MYSQL_DB')
