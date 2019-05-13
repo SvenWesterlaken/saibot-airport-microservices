97e45a6f37adc83aa7cb0e708b154cf624723365
Thimo Koolen <tkoolen22@gmail.com>
2019-05-13 13:35:31 +0200

    Added Taxiway model and modified Taxiway endpoints to support MongoDB operations

--- airside_management/routes/v1/taxiway.v1.js
+++ airside_management/routes/v1/taxiway.v1.js
@@ -2,42 +2,24 @@ const express = require('express');
 const router = express.Router();
 const amqpManager = require('../../events/amqp.manager');
 const uuid = require('uuid/v4');
+const Taxiway = require('../../models/taxiway.model');
 
 router.get('/', (req, res) => {
-	//TODO Get taxiways from database
-	//TODO Add ID
-	let payload = [
-		{
-			identifier: 'A',
-			from_point: 'A1',
-			to_point: 'A2'
-		},
-		{
-			identifier: 'A',
-			from_point: 'A2',
-			to_point: 'A3'
-		},
-		{
-			identifier: 'B',
-			from_point: 'B1',
-			to_point: 'B2'
-		}
-	];
-	
-	res.status(200).json({taxiways: payload});
+	Taxiway.find({})
+		.then((taxiways) => {
+			res.status(200).json(taxiways);
+		})
+		.catch((error) => res.status(400).json(error));
 });
 
 router.get('/:id', (req, res) => {
 	let id = req.params.id;
 	
-	//TODO Get taxiway from database
-	let payload = {
-		identifier: 'A',
-		from_point: 'A2',
-		to_point: 'A3'
-	};
-	
-	res.status(200).json(payload);
+	Taxiway.findOne({_id: id})
+		.then((taxiway) => {
+			res.status(200).json(taxiway);
+		})
+		.catch((error) => res.status(400).json(error));
 });
 
 router.post('/', (req, res) => {
@@ -48,100 +30,112 @@ router.post('/', (req, res) => {
 		res.status(400).json({error: 'Invalid request.'});
 	}
 	
-	//TODO Implement logic to add taxiway to database
-	
-	//Create message payload
-	let payload = {
-		id: uuid(),
-		message: 'New taxiway has been added successfully.',
-		from: 'airside_management',
-		type: 'CREATE',
-		data: {
-			identifier: body.identifier,
-			from_point: body.from_point,
-			to_point: body.to_point
-		},
-		old_data: {}
-	};
-	
-	amqpManager.connect()
-		.then((channel) => {
-			amqpManager.sendMessageToQueue(channel, 'airside-taxiway', JSON.stringify(payload));
-			res.status(201).json(payload);
+	Taxiway.create(body)
+		.then((taxiway) => {
+			//Create message payload
+			let payload = {
+				id: uuid(),
+				message: 'New taxiway has been added successfully.',
+				from: 'airside_management',
+				type: 'CREATE',
+				data: taxiway,
+				old_data: {}
+			};
+			
+			amqpManager.connect()
+				.then((channel) => {
+					amqpManager.sendMessageToQueue(channel, 'airside-taxiway', JSON.stringify(payload));
+					res.status(201).json(payload);
+				})
+				.catch((error) => {
+					res.status(400).json({
+						message: 'Unable to add new taxiway.',
+						error: error
+					});
+				});
 		})
-		.catch((error) => {
-			res.status(400).json({
-				message: 'Unable to add new taxiway.',
-				error: error
-			});
-		});
+		.catch((error) => res.status(200).json(error));
 });
 
 router.patch('/:id', (req, res) => {
 	let id = req.params.id;
 	let body = req.body;
 	
-	//TODO Get taxiway data from database, and patch
-	
-	let payload = {
-		id: uuid(),
-		message: 'Taxiway has been patched successfully.',
-		from: 'airside_management',
-		type: 'PATCH',
-		data: {
-			identifier: body.identifier,
-			from_point: body.from_point,
-			to_point: body.to_point
-		},
-		old_data: {
-			identifier: body.identifier,
-			from_point: body.from_point,
-			to_point: body.to_point
-		}
-	};
-	
-	amqpManager.connect()
-		.then((channel) => {
-			amqpManager.sendMessageToQueue(channel, 'airside-taxiway', JSON.stringify(payload));
-			res.status(201).json(payload);
+	Taxiway.findOne({_id: id})
+		.then((result) => {
+			let oldTaxiway = result;
+			let oldIdentifier = result.identifier;
+			let oldFromPoint = result.from_point;
+			let oldToPoint = result.to_point;
+			
+			if (body.identifier != null)
+				oldTaxiway.identifier = body.identifier;
+			
+			if (body.from_point != null)
+				oldTaxiway.from_point = body.from_point;
+			
+			if (body.to_point != null)
+				oldTaxiway.to_point = body.to_point;
+			
+			oldTaxiway.save()
+				.then((taxiway) => {
+					let payload = {
+						id: uuid(),
+						message: 'Taxiway has been patched successfully.',
+						from: 'airside_management',
+						type: 'PATCH',
+						data: taxiway,
+						old_data: {
+							identifier: oldIdentifier,
+							from_point: oldFromPoint,
+							to_point: oldToPoint
+						}
+					};
+					
+					amqpManager.connect()
+						.then((channel) => {
+							amqpManager.sendMessageToQueue(channel, 'airside-taxiway', JSON.stringify(payload));
+							res.status(201).json(payload);
+						})
+						.catch((error) => {
+							res.status(400).json({
+								message: 'Unable to patch taxiway.',
+								error: error
+							});
+						});
+				})
+				.catch((error) => res.status(400).json(error));
 		})
-		.catch((error) => {
-			res.status(400).json({
-				message: 'Unable to patch taxiway.',
-				error: error
-			});
-		});
+		.catch((error) => res.status(200).json(error));
 });
 
 router.delete('/:id', (req, res) => {
 	let id = req.params.id;
 	
-	//TODO Get taxiway data from database, and remove
-	
-	let payload = {
-		id: uuid(),
-		message: 'Taxiway has been deleted successfully.',
-		from: 'airside_management',
-		type: 'DELETE',
-		data: {},
-		old_data: {
-			identifier: 'A',
-			from_point: 'A2',
-			to_point: 'A3'
-		}
-	};
-	
-	amqpManager.connect()
-		.then((channel) => {
-			amqpManager.sendMessageToQueue(channel, 'airside-taxiway', JSON.stringify(payload));
-			res.status(201).json(payload);
+	Taxiway.findOneAndDelete({_id: id})
+		.then((taxiway) => {
+			let payload = {
+				id: uuid(),
+				message: 'Taxiway has been deleted successfully.',
+				from: 'airside_management',
+				type: 'DELETE',
+				data: {},
+				old_data: taxiway
+			};
+			
+			amqpManager.connect()
+				.then((channel) => {
+					amqpManager.sendMessageToQueue(channel, 'airside-taxiway', JSON.stringify(payload));
+					res.status(201).json(payload);
+				})
+				.catch((error) => {
+					res.status(400).json({
+						message: 'Unable to delete taxiway.',
+						error: error
+					});
+				});
 		})
-		.catch((error) => {
-			res.status(400).json({
-				message: 'Unable to delete taxiway.',
-				error: error
-			});
-		});
+		.catch((error) => res.status(400).json(error));
 });
 
 module.exports = router;
