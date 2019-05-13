08fc1d4b4e3cba5dc2533b8e0a62225a63094ad5
Thimo Koolen <tkoolen22@gmail.com>
2019-05-13 11:50:15 +0200

    Added fuel model and changed API endpoints to use database

--- airside_management/routes/v1/fuel.v1.js
+++ airside_management/routes/v1/fuel.v1.js
@@ -2,39 +2,24 @@ const express = require('express');
 const router = express.Router();
 const amqpManager = require('../../events/amqp.manager');
 const uuid = require('uuid/v4');
+const Fuel = require('../../models/fuel.model');
 
 router.get('/', (req, res) => {
-	//TODO Get current fuel levels from database
-	
-	let payload = [
-		{
-			fuel_level: 1000000,
-			fuel_capacity: 1000000
-		},
-		{
-			fuel_level: 943839,
-			fuel_capacity: 1000000
-		},
-		{
-			fuel_level: 32029,
-			fuel_capacity: 500000
-		}
-	];
-	
-	res.status(200).json({fuel_levels: payload});
+	Fuel.find({})
+		.then((containers) => {
+			res.status(200).json(containers);
+		})
+		.catch((error) => res.status(400).json(error));
 });
 
 router.get('/:id', (req, res) => {
 	let id = req.params.id;
 	
-	//TODO Get fuel tank usage from database
-	
-	let payload = {
-		fuel_level: 943839,
-		fuel_capacity: 1000000
-	};
-	
-	res.status(200).json(payload);
+	Fuel.findOne({_id: id})
+		.then((container) => {
+			res.status(200).json(container);
+		})
+		.catch((error) => res.status(400).json(error));
 });
 
 router.post('/', (req, res) => {
@@ -44,96 +29,105 @@ router.post('/', (req, res) => {
 		res.status(400).json({error: 'Invalid request.'});
 	}
 	
-	//TODO Add new fuel tank to the database
-	
-	//Create message payload
-	let payload = {
-		id: uuid(),
-		message: 'New fuel tank has been added successfully.',
-		from: 'airside_management',
-		type: 'CREATE',
-		data: {
-			fuel_level: body.fuel_level,
-			fuel_capacity: body.fuel_capacity
-		},
-		old_data: {}
-	};
-	
-	amqpManager.connect()
-		.then((channel) => {
-			amqpManager.sendMessageToQueue(channel, 'airside-fuel', JSON.stringify(payload));
-			res.status(201).json(payload);
+	Fuel.create(body)
+		.then((container) => {
+			let payload = {
+				id: uuid(),
+				message: 'New fuel tank has been added successfully.',
+				from: 'airside_management',
+				type: 'CREATE',
+				data: container,
+				old_data: {}
+			};
+			
+			amqpManager.connect()
+				.then((channel) => {
+					amqpManager.sendMessageToQueue(channel, 'airside-fuel', JSON.stringify(payload));
+					res.status(201).json(payload);
+				})
+				.catch((error) => {
+					res.status(400).json({
+						message: 'Unable to add new fuel tank.',
+						error: error
+					});
+				});
 		})
-		.catch((error) => {
-			res.status(400).json({
-				message: 'Unable to add new fuel tank.',
-				error: error
-			});
-		});
+		.catch((error) => res.status(400).json(error));
 });
 
 router.patch('/:id', (req, res) => {
 	let id = req.params.id;
 	let body = req.body;
 	
-	//TODO Get fuel tank data from database, and patch
-	
-	let payload = {
-		id: uuid(),
-		message: 'Fuel tank has been patched successfully.',
-		from: 'airside_management',
-		type: 'PATCH',
-		data: {
-			fuel_level: body.fuel_level,
-			fuel_capacity: body.fuel_capacity
-		},
-		old_data: {
-			fuel_level: body.fuel_level,
-			fuel_capacity: body.fuel_capacity
-		}
-	};
-	
-	amqpManager.connect()
-		.then((channel) => {
-			amqpManager.sendMessageToQueue(channel, 'airside-fuel', JSON.stringify(payload));
-			res.status(201).json(payload);
-		})
-		.catch((error) => {
-			res.status(400).json({
-				message: 'Unable to patch fuel tank.',
-				error: error
-			});
+	Fuel.findOne({_id: id})
+		.then((result) => {
+			let oldContainer = result;
+			let oldCapacity = oldContainer.fuel_capacity;
+			let oldLevel = oldContainer.fuel_level;
+			
+			if (body.fuel_capacity != null)
+				oldContainer.fuel_capacity = body.fuel_capacity;
+			
+			if (body.fuel_level != null) 
+				oldContainer.fuel_level = body.fuel_level;
+			
+			oldContainer.save()
+				.then((container) => {
+					let payload = {
+						id: uuid(),
+						message: 'Fuel tank has been patched successfully.',
+						from: 'airside_management',
+						type: 'PATCH',
+						data: container,
+						old_data: {
+							fuel_level: oldLevel,
+							fuel_capacity: oldCapacity
+						}
+					};
+					
+					amqpManager.connect()
+						.then((channel) => {
+							amqpManager.sendMessageToQueue(channel, 'airside-fuel', JSON.stringify(payload));
+							res.status(201).json(payload);
+						})
+						.catch((error) => {
+							res.status(400).json({
+								message: 'Unable to patch fuel tank.',
+								error: error
+							});
+						});
+				})
+				.catch((error) => res.status(400).json(error));
 		});
 });
 
 router.delete('/:id', (req, res) => {
 	let id = req.params.id;
 	
-	//TODO Get fuel tank data from database, and remove
-	
-	let payload = {
-		id: uuid(),
-		message: 'Fuel tank has been deleted successfully.',
-		from: 'airside_management',
-		type: 'PATCH',
-		data: {},
-		old_data: {
-			fuel_level: 943839,
-			fuel_capacity: 1000000
-		}
-	};
-	
-	amqpManager.connect()
-		.then((channel) => {
-			amqpManager.sendMessageToQueue(channel, 'airside-fuel', JSON.stringify(payload));
-			res.status(201).json(payload);
+	Fuel.findOneAndDelete({_id: id})
+		.then((container) => {
+			let payload = {
+				id: uuid(),
+				message: 'Fuel tank has been deleted successfully.',
+				from: 'airside_management',
+				type: 'DELETE',
+				data: {},
+				old_data: container
+			};
+			
+			amqpManager.connect()
+				.then((channel) => {
+					amqpManager.sendMessageToQueue(channel, 'airside-fuel', JSON.stringify(payload));
+					res.status(201).json(payload);
+				})
+				.catch((error) => {
+					res.status(400).json({
+						message: 'Unable to delete fuel tank.',
+						error: error
+					});
+				});
 		})
-		.catch((error) => {
-			res.status(400).json({
-				message: 'Unable to delete fuel tank.',
-				error: error
-			});
-		});
+		.catch((error) => res.status(400).json(error));
 });
 
 module.exports = router;
