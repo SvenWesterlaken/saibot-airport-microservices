0545b1b90b0018847dbc524640f797c343dcbff0
Twan van Maastricht <ajmaastr@avans.nl>
2019-05-13 15:13:50 +0200

    fixed bug in airline routes

--- airplane_management/app/models/airline.py
+++ airplane_management/app/models/airline.py
@@ -4,4 +4,4 @@ from .base import db, ParsingMixin
 
 class Airline(db.Entity, ParsingMixin):
     name = Required(str)
-    airplanes = Set('Airplane', cascade_delete=False)
+    airplanes = Set('Airplane')
--- airplane_management/app/routes/airlines.py
+++ airplane_management/app/routes/airlines.py
@@ -2,7 +2,7 @@ from app.routes import bp
 from ..util.dto import AirlineDto
 from flask_restplus import Resource
 from pony.orm import *
-from app.models import Airline
+from app.models import Airline as f_airline
 import json
 from flask import request
 
@@ -16,7 +16,7 @@ class Airline_list(Resource):
     @db_session
     @api.doc('list_of_registered_airlines')
     def get(self):
-        airlines = [f.to_dict() for f in Airline.select()]
+        airlines = [f.to_dict() for f in f_airline.select()]
         return json.dumps(airlines)
 
     @db_session
@@ -24,7 +24,7 @@ class Airline_list(Resource):
     @api.expect(_airline, validate=True)
     def post(self):
         airline = request.json
-        new_airline = Airline(**airline)
+        new_airline = f_airline(**airline)
         return json.dumps(new_airline)
 
 @api.route('/<public_id>')
@@ -35,17 +35,14 @@ class Airline(Resource):
     @db_session
     @api.doc('get an Airline')
     def get(self, public_id):
-        try:
-            flight = Airline[public_id]
-            return json.dumps(flight.to_dict())
-        except ObjectNotFound:
-            api.abort(404)
+        flight = f_airline[public_id]
+        return json.dumps(flight.to_dict())
 
     @db_session
     @api.doc('update an Airline')
     def put(self, public_id):
         airplane_update = request.json
-        airplane = Airline[public_id]
+        airplane = f_airline[public_id]
         airplane.update_props(airplane_update)
 
         return json.dumps(airplane.to_dict())
@@ -53,17 +50,7 @@ class Airline(Resource):
     @db_session
     @api.doc('delete an Airline')
     def delete(self, public_id):
-        flight = None
-        try:
-            flight = Airline[public_id]
-        except ObjectNotFound:
-            api.abort(404)
-
-        try:
-            flight[public_id].delete()
-        except ConstraintError:
-            api.abort(400, "Airline still has airplanes")
-        return json.dumps(flight.to_dict())
+        f_airline[public_id].delete()
 
 
 
