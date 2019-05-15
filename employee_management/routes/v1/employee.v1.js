d6bb10f70c671fa7b8bed5410574adaabb06eafd
Thimo Koolen <tkoolen22@gmail.com>
2019-05-15 11:54:58 +0200

    Added PATCH and DELETE for employees.

--- employee_management/routes/v1/employee.v1.js
+++ employee_management/routes/v1/employee.v1.js
@@ -51,4 +51,85 @@ router.post('/', (req, res) => {
 		.catch((error) => res.status(400).json(error));
 });
 
+router.patch('/:id', (req, res) => {
+	let id = req.params.id;
+	let body = req.body;
+	
+	Employee.findOne({_id: id})
+		.then((result) => {
+			let oldEmployee = result;
+			let oldFirstName = result.first_name;
+			let oldLastName = result.last_name;
+			let oldDob = result.dob;
+			
+			if (body.first_name != null)
+				oldEmployee.first_name = body.first_name;
+			
+			if (body.last_name != null)
+				oldEmployee.last_name = body.last_name;
+			
+			if (body.dob != null)
+				oldEmployee.dob = body.dob;
+			
+			oldEmployee.save()
+				.then((employee) => {
+					let payload = {
+						id: uuid(),
+						message: 'Employee has been patched successfully.',
+						from: 'employee_management',
+						type: 'PATCH',
+						data: employee,
+						old_data: {
+							first_name: oldFirstName,
+							last_name: oldLastName,
+							dob: oldDob
+						}
+					};
+					
+					amqpManager.connectRmq()
+						.then((channel) => {
+							amqpManager.sendMessageToQueue(channel, 'employee-management', JSON.stringify(payload));
+							res.status(201).json(payload);
+						})
+						.catch((error) => {
+							res.status(400).json({
+								message: 'Unable to patch employee.',
+								error: error
+							});
+						});
+				})
+				.catch((error) => res.status(400).json(error));
+		})
+		.catch((error) => res.status(400).json(error));
+});
+
+router.delete('/:id', (req, res) => {
+	let id = req.params.id;
+	
+	Employee.findOneAndDelete({_id: id})
+		.then((employee) => {
+			let payload = {
+				id: uuid(),
+				message: 'Employee has been deleted successfully.',
+				from: 'employee_management',
+				type: 'DELETE',
+				data: {},
+				old_data: employee
+			};
+			
+			amqpManager.connectRmq()
+				.then((channel) => {
+					amqpManager.sendMessageToQueue(channel, 'employee-management', JSON.stringify(payload));
+					res.status(201).json(payload);
+				})
+				.catch((error) => {
+					res.status(400).json({
+						message: 'Unable to delete employee.',
+						error: error
+					});
+				});
+		})
+		.catch((error) => res.status(400).json(error));
+});
+
 module.exports = router;
