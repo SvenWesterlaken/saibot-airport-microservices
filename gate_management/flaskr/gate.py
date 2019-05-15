15f0fb709ad188e7766dbce826afe559897949ad
matsgemmeke <matsgemmeke@gmail.com>
2019-05-15 12:05:31 +0200

    RabbitMQ messaging toegevoegd. Bevat geen consumers van events

--- gate_management/flaskr/gate.py
+++ gate_management/flaskr/gate.py
@@ -1,7 +1,8 @@
-import functools, json, arrow
+import functools, json, arrow, uuid
 from flask import Blueprint, request
 from models import Gate
 from pony.orm import *
+from rabbitmq import msg_handler
 
 bp = Blueprint('gate', __name__, url_prefix='/gate')
 
@@ -15,7 +16,7 @@ def get_all():
 @bp.route('/<id>', methods=['GET'])
 @db_session
 def get(id):
-    # Check if input it is a number
+    # Check if input is a number
     try:
         int(id)
     except ValueError:
@@ -40,12 +41,23 @@ def create():
 
     new_gate = Gate(**gate)
 
+    payload = {
+        'id': str(uuid.uuid4()),
+        'message': 'New gate has been added successfully',
+        'from': 'gate_management',
+        'type': 'CREATE',
+        'data': new_gate.to_dict(),
+        'old_data': {}
+    }
+
+    msg_handler.send_message_to_queue('gate', json.dumps(payload))
+
     return json.dumps(new_gate.to_dict())
 
 @bp.route('/<id>', methods=['PUT'])
 @db_session
 def update(id):
-    # Check if input it is a number
+    # Check if input is a number
     try:
         int(id)
     except ValueError:
@@ -61,14 +73,26 @@ def update(id):
 
     gate_update = json.loads(request.data.decode('UTF-8'))
     gate = Gate[id]
+    gate_old = gate.to_dict()  # Make dictionary of old gate object before updating it
     gate.update_props(gate_update)
+
+    payload = {
+        'id': str(uuid.uuid4()),
+        'message': 'Gate has been updated successfully',
+        'from': 'game_management',
+        'type': 'PUT',
+        'data': gate.to_dict(),
+        'old_data': gate_old
+    }
+
+    msg_handler.send_message_to_queue('gate', json.dumps(payload))
 
     return json.dumps(gate.to_dict())
 
 @bp.route('/<id>', methods=['DELETE'])
 @db_session
 def delete(id):
-    # Check if input it is a number
+    # Check if input is a number
     try:
         int(id)
     except ValueError:
@@ -83,6 +107,18 @@ def delete(id):
         return 'Gate with id ' + id + ' does not exist', 400
 
     gate = Gate[id]
+    gate_old = gate.to_dict()  # Make dictionary of old gate object before its deletion
     gate.delete()
+
+    payload = {
+        'id': str(uuid.uuid4()),
+        'message': 'Check-in counter has been deleted successfully',
+        'from': 'check_in_counter_management',
+        'type': 'DELETE',
+        'data': {},
+        'old_data': gate_old
+    }
+
+    msg_handler.send_message_to_queue('gate', json.dumps(payload))
 
     return 'Succeeded'
