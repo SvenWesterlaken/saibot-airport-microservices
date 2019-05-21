9463cc7999a809ee0b4bd02e587b0f82858a3c69
Twan van Maastricht <ajmaastr@avans.nl>
2019-05-21 15:00:36 +0200

    changes to docker-compose

--- passenger_integration/src/main/java/saibot/MyRouteBuilder.java
+++ passenger_integration/src/main/java/saibot/MyRouteBuilder.java
@@ -1,8 +1,11 @@
 package saibot;
 
+import domain.Passenger;
 import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonDataFormat;
+import org.apache.camel.model.dataformat.JsonLibrary;
 
 /**
  * A Camel Java DSL Router
@@ -14,6 +17,9 @@ public class MyRouteBuilder extends RouteBuilder {
      */
     public void configure() {
 
+        JsonDataFormat jsonFormat = new JsonDataFormat(JsonLibrary.XStream);
+        jsonFormat.setUnmarshalType(Passenger.class);
+
 
         from("direct:insert")
                 .log("Inserted new Passenger")
@@ -23,6 +29,14 @@ public class MyRouteBuilder extends RouteBuilder {
         from("timer://simpleTimer?period=10000")
                 .to("sqlComponent:{{sql.getPassengers}}")
                 .bean("passengerMapper", "readPassengers")
+                .setHeader(Exchange.HTTP_METHOD, simple("POST"))
+                .setHeader(Exchange.CONTENT_TYPE, constant("application/json")).to("http://localhost:8080/employee")
+                .log("${body}");
+
+
+        from("timer://simpleTimer?period=10000")
+                .to("sqlComponent:{{sql.getPassengers}}")
+                .marshal(jsonFormat)
                 .setHeader(Exchange.HTTP_METHOD, simple("POST"))
                 .setHeader(Exchange.CONTENT_TYPE, constant("application/json")).to("http://localhost:8080/employee")
                 .log("${body}");
